8c6103a17b309b767e0cdf9e64d9c4a4f48a8519
Gaston Gaitan <gaston-gaitan@hotmail.com>
2024-09-24 00:01:21 -0300

    processing message feature

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 import openai
+import os
+from dotenv import load_dotenv  # Asegúrate de importar la librería
+from twilio.rest import Client
+
+# Cargar las variables desde el archivo .env
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -10,9 +16,26 @@ model = "gpt-4o"
 
 @app.route('/', methods=['GET', 'POST'])
 def process_message():
-    response = MessagingResponse()
-    response.message('Hola mundo desde el server de Flask de twilio')
-    return str(response)
+    if request.method == 'POST':
+        account_sid = os.environ['ACCOUNT_SID']
+        auth_token = os.environ['AUTH_TOKEN']
+
+        # agregar manejo de errores
+        data = request.get_json()
+
+        client = Client(account_sid, auth_token)
+        msg_to = data['msg_to']
+        msg_body = data['msg_body']
+        from_whatsapp_number = 'whatsapp:' + os.environ["TWILIO_NUMBER"]
+
+        message = client.messages.create(
+            from_=from_whatsapp_number,
+            body=msg_body,
+            to=msg_to
+        )
+
+        print(message.sid)
+        return 'Message sent to {}'.format(msg_to) 
 
 if __name__ == '__main__':
     app.run(debug=True)
