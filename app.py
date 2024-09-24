3a2f078865059f96fa45b46d424c9f23d357b3a8
Gaston Gaitan <gaston-gaitan@hotmail.com>
2024-09-23 23:35:46 -0300

    reading the post method data

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+import openai
 
 app = Flask(__name__)
 
+client = openai.OpenAI(api_key="sk-svcacct-ixqcwWtpjxabp64J4csqUGSqAl_bEr6VuRx-fJjbZx5p46EQp8BJE5li9er7yitaiayk0_YBc8RiUT3BlbkFJ9aT6iWGDSFldCuId9u_VcVskvRTJfPuK4SkzN3pyjgIyAezc_s_CrgWmr4glg3R9_oe8mK-uyoTjAA")
+
+model = "gpt-4o"
+
 @app.route('/', methods=['GET', 'POST'])
 def process_message():
+    if request.method == 'POST':
+        # Si los datos se envían como formulario
+        data = request.form
+
+        # O si se envían como JSON
+        json_data = request.get_json()
+
+        # Mostrar los datos recibidos
+        print(f'Datos del formulario: {data}')
+        print(f'Datos JSON: {json_data}')
     response = MessagingResponse()
     response.message('Hola mundo desde el server de Flask de twilio')
     return str(response)
