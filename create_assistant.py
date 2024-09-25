068be1bfb2804af18e70142e68709256f0a6c6c6
Gaston Gaitan <gaston-gaitan@hotmail.com>
2024-09-25 01:46:10 -0300

    adding twilio chars limit "Unable to create record: The concatenated message body exceeds the 1600 character limit"

--- create_assistant.py
+++ create_assistant.py
@@ -15,27 +15,31 @@ def read_json(file_path):
         data = json.load(file)  # Carga el contenido del archivo JSON en un diccionario
         return data
 
-kavak_data = read_json("kavak_data/sample_caso_ai_engineer.json")
+def read_txt(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = file.read()  # Carga el contenido del archivo de texto
+        return data
 
-print(kavak_data)
+kavak_cars_data = read_json("kavak_data/sample_caso_ai_engineer.json")
+kavak_business_data = read_txt("kavak_data/informacion_de_kavak.txt")
 
 # Assistant creation
 kavak_ai_assistant = client.beta.assistants.create(
+    assistant_id=os.environ["KAVAK_ASSISTANT_ID"], 
     name="Asistente de IA de Kavak",
     instructions=f'''
     Asistente de IA de Kavak para ayudar con atención al cliente.
-    Kavak es una empresa de venta de autos usados en línea de la más alta calidad.
+    Vas al grano, no te extiendas demasiado en tus respuestas.
+    Tus respuestas no pueden exceder las 500 palabras. Siempre trata de ser lo mas breve posible.
+    Esta es informacion de la empresa y el modelo de negocio de Kavak: {kavak_business_data}
     No puedes involucrarte en ninguna convesarción que no tenga que ver con autos y el negocio de Kavak.
-    Estas entrenado con información de los autos  de Kavak.
-    Debes buscar cerrar ventas de autos y dar información sobre los autos de Kavak.
     Dependiendo del auto por el que la persona se sienta interesada, seras capaz de otorgar 
     planes de financiamiento tomando como base el enganche, el precio
     del auto, una tasa de interés del 10% y plazos de financiamiento de entre 3 y 6 años.
-    Informacion de nuestros vehiculos disponibles de los cuales deberas proporcionar informacion: ꐕ{kavak_data}ꐕ
+    Informacion de nuestros vehiculos disponibles de los cuales deberas proporcionar informacion y buscar cerrar ventas: ꐕ{kavak_cars_data}ꐕ
     Importante, solamente brindaras informacion de los autos que se encuentran en el json delimitado entre los simbolos ꐕ.
     ''',
     model="gpt-4o",
-    tools=[{"type":"file_search"}]
 )
 
 kavak_assistant_id = kavak_ai_assistant.id
